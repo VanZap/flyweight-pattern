2c8575a972cf1cdb1b8f8e84dabf8585b853d99e
IvanZapanta <ivanzapanta@gmail.com>
2025-11-01 00:28:04 -0400

    add MVC

---
+++ flyweight.py
@@ -0,0 +1,34 @@
+import time
+
+class Order:
+    def __init__(self, name):
+        self.name = name
+        print(f"Cooking {name} order...")
+        time.sleep(3)  # simulate time-consuming creation
+        print(f"Done cooking {name} order!")
+
+class RestaurantView:
+    def show_order(self, order):
+        print(f"Order for: {order.name} | ID: {id(order)}\n")
+
+    def show_message(self, message):
+        print(message)
+
+class RestaurantController:
+    def __init__(self):
+        self.view = RestaurantView()
+
+    def create_order(self, food_name):
+        order = Order(food_name)
+        self.view.show_order(order)
+        return order
+
+if __name__ == "__main__":
+    controller = RestaurantController()
+    pizza1 = controller.create_order("Pizza")
+    pizza2 = controller.create_order("Pizza")
+    pizza3 = controller.create_order("Pizza")
+
+    burger1 = controller.create_order("Burger")
+    burger2 = controller.create_order("Burger")
+    burger3 = controller.create_order("Burger")
